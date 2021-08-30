17c4e552132232c4fb1d424534b900dbeee56c8c
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-08-30 23:09:34 +0530

    google

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/quote",
+      callbackURL: `${process.env.URL}/auth/google/quote`,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
